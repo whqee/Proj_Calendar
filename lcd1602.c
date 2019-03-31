3ee46173c7190624f2daf385a23395a6729f05b7
whqee <whqee@qq.com>
2019-03-31 17:52:17 +0800

    完成初版万年历。24小时制，不分大小月、闰年.

--- lcd1602.c
+++ lcd1602.c
@@ -6,7 +6,6 @@ sbit lcd1602_E	 = P2^7;
 sbit lcd1602_RW = P2^6;			
 sbit lcd1602_RS = P2^5;			
 
-static u8 x = 0, y = 0;
 static void delay5ms(void);
 static void lcd1602_wait_noBusy(void);
 static void lcd1602_write_cmd(u8 cmd);
@@ -98,16 +97,13 @@ void lcd1602_show_str(u8 x, u8 y, u8 *pStr, u8 size)
 
 void lcd1602_print(u8 *pStr)     
 {
-    lcd1602_set_cursor(x, y);      
+	u8 x = 0;
+    lcd1602_set_cursor(x, 0);      
     while (*pStr != '\0')
     {
         lcd1602_write_data(*pStr++);
-			  if(x++ == 15)
-				{
-					x = 0;
-					y = 1;
-				}
-				lcd1602_set_cursor(x, y);
+			if(x++ == 15)
+				lcd1602_set_cursor(0, 1);
     }
 }
 
--- main.c
+++ main.c
@@ -1,18 +1,17 @@
 #include <common.h>
 #include <lcd1602.h>
 #include <ds1302.h>
-#include <stdio.h>
 
-#define BCD_TO_BIN(val)   ((((val) >> 4) * 10) +  ((val)&15))
-#define BCD_TO_ASCCII(val)    (((((val) >> 8) + 0x30) << 8) +  (((val) & 0xFF) + 0x30))
+#define BCD_TO_BIN(val)   ( (((val) >> 4) * 10) +  ((val)&15) )
+#define BIN_TO_BCD(val)   ( (((val)/10) << 4) + (((val)%10)) )
 
 sbit KEY0 = P3^2;
 sbit KEY1 = P3^3;
 
 
-u8 i = 0, k;
-static u8 count = 0;
-static char time_now[7];
+u8 i = 0, xdata tmp; 
+static u8 xdata count = 0;
+static char xdata time_now[7];
 static char time_ascii[14];
 
 //functions prototype:
@@ -26,9 +25,9 @@ void main()
 	lcd1602_Init();	
 	
 	/** KEY_Init: Set KEY0 Interrupt **/
-	IT0 = 1;			// Falling edge trigger
-	EX0 = 1;			// Enable EX0 Interrupt line
-	IT1 = 1;
+	IT0 = 1;			// KEY0 Falling edge trigger
+	IT1 = 1;			// KEY1 Falling edge trigger
+	EX0 = 1;			// Enable KEY0 Interrupt line
 	EX1 = 0;
 	EA = 1;				// Enable Interrupt
 	/** End setting KEY0 Interrupt **/
@@ -47,90 +46,152 @@ void main()
 		Set_Time:
 		if(!count)
 			goto Normal_Running;
+
 		while (count) {
 			switch (count) {
-				case 13:
-					k = 12;
-					time_ascii[k] = time_now[k/2] >>4 & 63 | 48;  lcd1602_show_str(2,0, &time_ascii[k], 1); 
-					for(i=11; i > 0; i--)delay_50ms();  if(KEY1 == 0) time_now[k/2] += 16;  if((time_now[k/2]>>4) > 9) time_now[k/2] &= 15;
+			case 1:
+				tmp = BCD_TO_BIN(time_now[count-1]);
+				if (KEY1 == 0) {
+					tmp++;
+					if (tmp > 59) tmp = 0;	//modify here         1
+					time_now[count-1] = BIN_TO_BCD(tmp);
+				}
+				// update display
+				EA = 0; // Disable Interrupt
+				lcd1602_show_str(6,1, "  ", 2);  // modify here (x,y)          2
+				for(i=2; i > 0; i--) delay_50ms();
+				time_ascii[2*count-1] = time_now[count-1] & 63 | 48;    // BCD TO ASCII.
+				time_ascii[2*count-2] = time_now[count-1] >>4 &63 | 48; // BCD TO ASCII.
+				lcd1602_show_str(6,1, &time_ascii[2*count-2], 2); // modify here (x,y)         3
+				EA = 1;		//Enable Interrupt
+				for(i=2; i > 0; i--) delay_50ms();
 				break;
-				case 12:
-					k = 12;
-					time_ascii[k] = time_now[k/2] & 63 | 48;  lcd1602_show_str(3,0, &time_ascii[k+1], 1); 
-					for(i=11; i > 0; i--)delay_50ms();  if(KEY1 == 0) time_now[k/2] += 1;  if(time_now[k/2] & 15 > 9) time_now[k/2] &= 240; // 240 is 1111 0000b
+			case 2:
+				tmp = BCD_TO_BIN(time_now[count-1]);
+				if (KEY1 == 0) {
+					tmp++;
+					if (tmp > 59) tmp = 0;	//modify here         1
+					time_now[count-1] = BIN_TO_BCD(tmp);
+				}
+				// update display
+				EA = 0; // Disable Interrupt
+				lcd1602_show_str(3,1, "  ", 2);  // modify here (x,y)           2
+				for(i=2; i > 0; i--) delay_50ms();
+				time_ascii[2*count-1] = time_now[count-1] & 63 | 48;    // BCD TO ASCII.
+				time_ascii[2*count-2] = time_now[count-1] >>4 &63 | 48; // BCD TO ASCII.
+				lcd1602_show_str(3,1, &time_ascii[2*count-2], 2); // modify here (x,y)          3
+				EA = 1;		//Enable Interrupt
+				for(i=2; i > 0; i--) delay_50ms();
 				break;
-
-				case 11:
-					k = 8;
-					time_ascii[k] = time_now[k/2] >>4 & 63 | 48;  lcd1602_show_str(5,0, &time_ascii[k], 1); 
-					for(i=11; i > 0; i--)delay_50ms();  if(KEY1 == 0) time_now[k/2] += 16;  if((time_now[k/2]>>4) > 1) time_now[k/2] &= 15;
+			case 3:
+				tmp = BCD_TO_BIN(time_now[count-1]);
+				if (KEY1 == 0) {
+					tmp++;
+					if (tmp > 23) tmp = 0;	//modify here          1
+					time_now[count-1] = BIN_TO_BCD(tmp);
+				}
+				// update display
+				EA = 0; // Disable Interrupt
+				lcd1602_show_str(0,1, "  ", 2);  // modify here (x,y)          2
+				for(i=2; i > 0; i--) delay_50ms();
+				time_ascii[2*count-1] = time_now[count-1] & 63 | 48;    // BCD TO ASCII.
+				time_ascii[2*count-2] = time_now[count-1] >>4 &63 | 48; // BCD TO ASCII.
+				lcd1602_show_str(0,1, &time_ascii[2*count-2], 2); // modify here (x,y)           3
+				EA = 1;		//Enable Interrupt
+				for(i=2; i > 0; i--) delay_50ms();
 				break;
-				case 10:
-					k = 8;
-					time_ascii[k] = time_now[k/2] & 63 | 48;  lcd1602_show_str(6,0, &time_ascii[k+1], 1); 
-					for(i=11; i > 0; i--)delay_50ms();  if(KEY1 == 0) time_now[k/2] += 1;  if(time_now[k/2] & 15 > 9) time_now[k/2] &= 240; // 240 is 1111 0000b
-				break;
-
-				case 9:
-					k = 6;
-					time_ascii[k] = time_now[k/2] >>4 & 63 | 48;  lcd1602_show_str(8,0, &time_ascii[k], 1); 
-					for(i=11; i > 0; i--)delay_50ms();  if(KEY1 == 0) time_now[k/2] += 16;  if((time_now[k/2]>>4) > 3) time_now[k/2] &= 15;
-				break;
-				case 8:
-					k = 6;
-					time_ascii[k] = time_now[k/2] & 63 | 48;  lcd1602_show_str(9,0, &time_ascii[k+1], 1); 
-					for(i=11; i > 0; i--)delay_50ms();  if(KEY1 == 0) time_now[k/2] += 1;  if(time_now[k/2] & 15 > 9) time_now[k/2] &= 240; // 240 is 1111 0000b
-				break;
-
-				case 7:
-					k = 10;
-					time_ascii[k] = time_now[k/2] & 63 | 48;  lcd1602_show_str(11,0, &time_ascii[k+1], 1); 
-					for(i=11; i > 0; i--)delay_50ms();  if(KEY1 == 0) time_now[k/2] += 1;  if(time_now[k/2] & 15 > 7) time_now[k/2] &= 240; // 240 is 1111 0000b
-				break;
-
-				case 6:
-					k = 4;
-					time_ascii[k] = time_now[k/2] >>4 & 63 | 48;  lcd1602_show_str(0,1, &time_ascii[k], 1); 
-					for(i=11; i > 0; i--)delay_50ms();  if(KEY1 == 0) time_now[k/2] += 16;  if((time_now[k/2]>>4) > 2) time_now[k/2] &= 15;
-				break;
-				case 5:
-					k = 4;
-					time_ascii[k] = time_now[k/2] & 63 | 48;  lcd1602_show_str(1,1, &time_ascii[k+1], 1); 
-					for(i=11; i > 0; i--)delay_50ms();  if(KEY1 == 0) time_now[k/2] += 1;  if(time_now[k/2] & 15 > 9) time_now[k/2] &= 240; // 240 is 1111 0000b
-				break;
-
-				case 4:
-					k = 2;
-					time_ascii[k] = time_now[k/2] >>4 & 63 | 48;  lcd1602_show_str(3,1, &time_ascii[k], 1); 
-					for(i=11; i > 0; i--)delay_50ms();  if(KEY1 == 0) time_now[k/2] += 16;  if((time_now[k/2]>>4) > 6) time_now[k/2] &= 15;
-				break;
-				case 3:
-					k = 2;
-					time_ascii[k] = time_now[k/2] & 63 | 48;  lcd1602_show_str(4,1, &time_ascii[k+1], 1); 
-					for(i=11; i > 0; i--)delay_50ms();  if(KEY1 == 0) time_now[k/2] += 1;  if(time_now[k/2] & 15 > 9) time_now[k/2] &= 240; // 240 is 1111 0000b
-				break;
-
-				case 2:
-					k = 0;
-					time_ascii[k] = time_now[k/2] >>4 & 63 | 48;  lcd1602_show_str(6,1, &time_ascii[k], 1); 
-					for(i=11; i > 0; i--)delay_50ms();  if(KEY1 == 0) time_now[k/2] += 16;  if((time_now[k/2]>>4) > 6) time_now[k/2] &= 15;
-				break;
+			case 4:
+				tmp = BCD_TO_BIN(time_now[count-1]);
+				if (KEY1 == 0) {
+					tmp++;
+					if (tmp > 7) tmp = 1;	//modify here             1 
+					time_now[count-1] = BIN_TO_BCD(tmp);
+				}
+				// update display
+				EA = 0; // Disable Interrupt
+				lcd1602_show_str(10,0, "  ", 2);  // modify here (x,y)           2
+				for(i=2; i > 0; i--) delay_50ms();
+				time_ascii[2*count-1] = time_now[count-1] & 63 | 48;    // BCD TO ASCII.
+				time_ascii[2*count-2] = time_now[count-1] >>4 &63 | 48; // BCD TO ASCII.
+				switch ((time_now[3] & 15)) {             //modify here  (x,y)                 3-8
 				case 1:
-					k = 0;
-					time_ascii[k] = time_now[k/2] & 63 | 48;  lcd1602_show_str(7,1, &time_ascii[k+1], 1); 
-					for(i=11; i > 0; i--)delay_50ms();  if(KEY1 == 0) time_now[k/2] += 1;  if(time_now[k/2] & 15 > 9) time_now[k/2] &= 240; // 240 is 1111 0000b
+					lcd1602_show_str(11,0, "1 Mon", 5); break;
+				case 2:
+					lcd1602_show_str(11,0, "2 Tue", 5); break;
+				case 3:
+					lcd1602_show_str(11,0, "3 Wed", 5); break;
+				case 4:
+					lcd1602_show_str(11,0, "4 Thu", 5); break;
+				case 5:
+					lcd1602_show_str(11,0, "5 Fri", 5); break;
+				case 6:
+					lcd1602_show_str(11,0, "6 Sat", 5); break;
+				case 7:
+					lcd1602_show_str(11,0, "7 Sun", 5); break;
+				default:
+					lcd1602_show_str(11,0, "error", 5);
+					break;
+				}
+				EA = 1;		//Enable Interrupt
+				for(i=2; i > 0; i--) delay_50ms();
+				break;
+			case 5:
+				tmp = BCD_TO_BIN(time_now[count-1]);
+				if (KEY1 == 0) {
+					tmp++;
+					if (tmp > 31) tmp = 1;	//modify here                 1
+					time_now[count-1] = BIN_TO_BCD(tmp);
+				}
+				// update display
+				EA = 0; // Disable Interrupt
+				lcd1602_show_str(8,0, "  ", 2);  // modify here (x,y)             2
+				for(i=2; i > 0; i--) delay_50ms();
+				time_ascii[2*count-1] = time_now[count-1] & 63 | 48;    // BCD TO ASCII.
+				time_ascii[2*count-2] = time_now[count-1] >>4 &63 | 48; // BCD TO ASCII.
+				lcd1602_show_str(8,0, &time_ascii[2*count-2], 2); // modify here (x,y)       3
+				EA = 1;		//Enable Interrupt
+				for(i=2; i > 0; i--) delay_50ms();
+				break;
+			case 6:
+				tmp = BCD_TO_BIN(time_now[count-1]);
+				if (KEY1 == 0) {
+					tmp++;
+					if (tmp > 12) tmp = 1;	//modify here            1
+					time_now[count-1] = BIN_TO_BCD(tmp);
+				}
+				// update display
+				EA = 0; // Disable Interrupt
+				lcd1602_show_str(5,0, "  ", 2);  // modify here (x,y)            2
+				for(i=2; i > 0; i--) delay_50ms();
+				time_ascii[2*count-1] = time_now[count-1] & 63 | 48;    // BCD TO ASCII.
+				time_ascii[2*count-2] = time_now[count-1] >>4 &63 | 48; // BCD TO ASCII.
+				lcd1602_show_str(5,0, &time_ascii[2*count-2], 2); // modify here (x,y)       3
+				EA = 1;		//Enable Interrupt
+				for(i=2; i > 0; i--) delay_50ms();
+				break;
+			case 7:
+				tmp = BCD_TO_BIN(time_now[count-1]);
+				if (KEY1 == 0) {
+					tmp++;
+					if (tmp > 99) tmp = 0;     // modify here         1
+					time_now[count-1] = BIN_TO_BCD(tmp);
+				}
+				// update display
+				EA = 0; // Disable Interrupt
+				lcd1602_show_str(2,0, "  ", 2); // modify here (x,y)             2
+				for(i=2; i > 0; i--) delay_50ms();
+				time_ascii[2*count-1] = time_now[count-1] & 63 | 48;    // BCD TO ASCII
+				time_ascii[2*count-2] = time_now[count-1] >>4 &63 | 48; // BCD TO ASCII
+				lcd1602_show_str(2,0, &time_ascii[2*count-2], 2); // modify here (x,y)        3
+				EA = 1;		//Enable Interrupt
+				for(i=2; i > 0; i--) delay_50ms();
+				break;
+			default:
+				lcd1602_show_str(11,0, "error", 5);
 				break;
 			}
-			ds1302_set_time(time_now);
 		}
-		//time_now[0] = 0x50;
-		//time_now[1] = 0x59;
-		//time_now[2] = 0x23;
-		//time_now[3] = 0x23;
-		//time_now[4] = 0x10;
-		//time_now[5] = 0x01;
-		//time_now[6] = 0x17;
-		//ds1302_set_time(time_now);
+		ds1302_set_time(time_now);
 	}
 	
 }
@@ -142,10 +203,12 @@ static void print_time()
 	lcd1602_show_str(0,0, "20", 2);
 	while(j--)
 	{
+		// BCD_TO_ASCII
 		time_ascii[2*j+1] = time_now[j] & 63 | 48;
 		time_ascii[2*j] = time_now[j] >>4 &63 | 48;
 
-		if (y == 0 && x <= 8) {
+		// print: year.mon.date
+		if (j > 3) {
 			lcd1602_show_str(x,y, &time_ascii[2*j], 2);
 			x += 2;
 			if (x > 8) 
@@ -153,10 +216,13 @@ static void print_time()
 			else
 				lcd1602_show_str(x,y, ".", 2);
 			x += 1;
+
+			
 		}
 
-		if (y == 0 && x == 11) {
-			switch ((time_ascii[2*j+1] & 15)) {
+		//print: day
+		if (j == 3) {
+			switch ((time_now[j] & 15)) {
 			case 1:
 				lcd1602_show_str(x,y, "1 Mon", 5); break;
 			case 2:
@@ -172,13 +238,16 @@ static void print_time()
 			case 7:
 				lcd1602_show_str(x,y, "7 Sun", 5); break;
 			default:
-				lcd1602_show_str(x,y, &time_ascii[2*j+1], 1); break;
+				ds1302_set_time_once(j, 1);
+				lcd1602_show_str(x,y, "error", 5);
+				break;
 			}
 			x = 0;
 			y = 1;
 		}
 		
-		if (y == 1 && x < 9) {
+		// print: hours:min:sec
+		if (j < 3) {
 			lcd1602_show_str(x,y, &time_ascii[2*j], 2);
 			x += 2;
 			if (x < 6)
@@ -188,7 +257,7 @@ static void print_time()
 		
 		
 	}
-	j = 7; x = 2; y =0;
+	// lcd1602_print(time_ascii);
 	
 }
 
@@ -204,13 +273,15 @@ void KEY_Line() interrupt 0
 {
 	u8 i = 0;
 	delay_50ms();
-	if(!count)
-		count = 13;
-	else 
-		count--;
+	if (KEY0 == 0) {
+		if(!count)
+			count = 7;
+		else 
+			count--;
 
-	for(i=11; i > 0; i--)
+		for(i=11; i > 0; i--)
 			delay_50ms();
+	}
 }
 
 
